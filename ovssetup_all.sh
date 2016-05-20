8c44a0d2c54790020cb40725647cc39ea2795ad6
Nimay <ntdesai2@illinois.edu>
2016-05-20 09:51:07 -0700

    Adding wrapper script to take care of entire ovs setup.

--- ovssetup_all.sh
+++ ovssetup_all.sh
@@ -1,7 +1,10 @@
 git clone https://github.com/openvswitch/ovs
 cp ovssetup.sh ovs
 mv ovs ..
+cd ../ovs
 
-../ovs/ovssetup.sh conf
-../ovs/ovssetup.sh make
-../ovs/ovssetup.sh run
+./ovssetup.sh conf
+./ovssetup.sh make
+./ovssetup.sh run
+
+cd -
