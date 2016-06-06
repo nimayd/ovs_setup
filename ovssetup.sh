4e8c01accb5312e4b537301948518e73973660f3
Nimay Desai <ntdesai2@illinois.edu>
2016-06-06 09:34:24 -0700

    Updating path in run

--- ovssetup.sh
+++ ovssetup.sh
@@ -8,7 +8,7 @@ then
     aptitude install python-qt4 python-qt4-dev pyqt-tools
     apt-get -y install git-email libmail-sendmail-perl libmailtools-perl
     ./boot.sh
-    ./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc --enable-ssl --enable-Werror #--with-linux=/lib/modules/`uname -r`/build
+    ./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc --enable-ssl --enable-Werror --with-linux=/lib/modules/`uname -r`/build
 
 elif [ "$COMMAND" == "make" ]
 then
@@ -16,12 +16,13 @@ then
     make install
     make modules_install
     /sbin/modprobe openvswitch
-    mkdir -p /usr/local/etc/openvswitch
-    ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+    mkdir -p /etc/openvswitch
+    ovsdb-tool create /etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
 
 elif [ "$COMMAND" == "run" ]
 then
-    ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+#    ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+    ovsdb-server --remote=punix:/var/run/openvswitch/db.sock \
                          --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
                          --private-key=db:Open_vSwitch,SSL,private_key \
                          --certificate=db:Open_vSwitch,SSL,certificate \
